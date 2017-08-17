43da710e3036e55670262ea7bc9912b957643033
Sijie Wang <sijiewang@sijiewang-macbookpro1.roam.corp.google.com>
2017-08-17 15:29:50 -0700

    Added Emacs config.

---
+++ .emacs.d/init.el
@@ -0,0 +1,67 @@
+
+(require 'package) ;; You might already have this line
+(add-to-list 'package-archives
+             '("melpa" . "https://melpa.org/packages/"))
+
+(when (< emacs-major-version 24)
+  ;; For important compatibility libraries like cl-lib
+  (add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/")))
+
+
+;; Make sure all my required packages are installed.
+; list the packages you want
+(setq package-list '(icicles))
+
+; list the repositories containing them
+(setq package-archives '(("elpa" . "http://tromey.com/elpa/")
+                         ("gnu" . "http://elpa.gnu.org/packages/")
+                         ("melpa" . "https://melpa.org/packages/")
+                         ("marmalade" . "http://marmalade-repo.org/packages/")))
+
+; activate all the packages (in particular autoloads)
+(package-initialize)
+
+; fetch the list of packages available 
+(unless package-archive-contents
+  (package-refresh-contents))
+
+; install the missing packages
+(dolist (package package-list)
+  (unless (package-installed-p package)
+    (package-install package)))
+
+
+
+
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+(package-initialize)
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(abbrev-mode t t)
+ '(auto-save-default nil)
+ '(comment-column 2)
+ '(current-language-environment "UTF-8")
+ '(default-input-method "latin-1-prefix")
+ '(ffap-newfile-prompt t)
+ '(font-lock-maximum-decoration 2)
+ '(indent-tabs-mode nil)
+ '(indicate-empty-lines nil)
+ '(initial-major-mode (quote text-mode))
+ '(initial-scratch-message "")
+ '(package-selected-packages (quote (typescript-mode icicles))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+
+(icy-mode 1)
