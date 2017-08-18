92235633c3ad335666ad92a62a52f656bdc617fa
armymen <wangsj.xm@gmail.com>
2017-08-17 18:09:57 -0700

    Some Emacs customization.

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,5 +1,14 @@
 
 (require 'package) ;; You might already have this line
+
+(global-set-key (kbd "C-x <up>") 'windmove-up)
+(global-set-key (kbd "C-x <down>") 'windmove-down)
+(global-set-key (kbd "C-x <right>") 'windmove-right)
+(global-set-key (kbd "C-x <left>") 'windmove-left)
+
+(require 'ng2-mode)
+
+
 (add-to-list 'package-archives
              '("melpa" . "https://melpa.org/packages/"))
 
@@ -58,13 +67,20 @@
  '(indicate-empty-lines nil)
  '(initial-major-mode (quote text-mode))
  '(initial-scratch-message "")
- '(package-selected-packages (quote (typescript-mode icicles))))
+ '(package-selected-packages (quote (ng2-mode typescript-mode icicles))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(default ((t (:inherit nil :stipple nil :background "#2e3436" :foreground "#eeeeec" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 130 :width normal :foundry "nil" :family "Menlo")))))
 
+
+
+
+(defun prev-window ()
+  (interactive)
+  (other-window -1))
 
 (icy-mode 1)
+
